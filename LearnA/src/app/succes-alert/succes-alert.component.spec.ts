fefa47bafa8c7b1464a788fcb4d8942d30617f40
TreacheryMD <draganel_ion@yahoo.com>
2017-07-06 15:26:23 +0300

    first assigment

---
+++ LearnA/src/app/succes-alert/succes-alert.component.spec.ts
@@ -0,0 +1,28 @@
+/* tslint:disable:no-unused-variable */
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { DebugElement } from '@angular/core';
+
+import { SuccesAlertComponent } from './succes-alert.component';
+
+describe('SuccesAlertComponent', () => {
+  let component: SuccesAlertComponent;
+  let fixture: ComponentFixture<SuccesAlertComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ SuccesAlertComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(SuccesAlertComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
