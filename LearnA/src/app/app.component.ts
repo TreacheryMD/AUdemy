6d006d8a0959d92d5ec50bab744399e23245521e
TreacheryMD <draganel_ion@yahoo.com>
2017-07-07 14:40:15 +0300

    finish

--- LearnA/src/app/app.component.ts
+++ LearnA/src/app/app.component.ts
@@ -6,4 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  showDetails = false;
+  log = [];
+
+  onToggleDetais() {
+    this.showDetails = !this.showDetails;
+    this.log.push(this.log.length + 1);
+  }
 }
