85bbaa3c5f97058bbe4c9e575cec4f4a35a45c7c
TreacheryMD <draganel_ion@yahoo.com>
2017-07-12 12:29:20 +0300

    recipes l

--- Project/src/app/app.module.ts
+++ Project/src/app/app.module.ts
@@ -11,7 +11,6 @@ import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.com
 import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
-import { IngridientComponent } from './shared/ingridient/ingridient.component';
 
 @NgModule({
   declarations: [
@@ -22,8 +21,7 @@ import { IngridientComponent } from './shared/ingridient/ingridient.component';
     RecipeDetailComponent,
     RecipeItemComponent,
     ShoppingListComponent,
-    ShoppingEditComponent,
-    IngridientComponent
+    ShoppingEditComponent
   ],
   imports: [
     BrowserModule,
