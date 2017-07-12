31806b3b80436609c3a0e9a79bf9f091dee2f03c
TreacheryMD <draganel_ion@yahoo.com>
2017-07-12 13:56:13 +0300

    dddd

--- Project/src/app/recipes/recipe-list/recipe-list.component.ts
+++ Project/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -9,12 +9,17 @@ import { Recipe } from '../recipe.model';
 export class RecipeListComponent implements OnInit {
   recipes: Recipe[] = [
     new Recipe('A Test Recipe', 'This is simply a test', 'https://goo.gl/f8XW12'),
-    new Recipe('A Test Recipe', 'This is simply a test', 'https://goo.gl/f8XW12')
+    new Recipe('Another Test Recipe', 'This is simply a test', 'https://goo.gl/f8XW12')
   ];
+
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeWasSelected.emit(recipe);
+  }
 }
